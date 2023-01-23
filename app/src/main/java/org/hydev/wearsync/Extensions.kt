9528d28bd09216436cd0b51d4abc7398e376ece8
Azalea Gui <me@hydev.org>
2023-01-23 16:55:28 -0500

    [O] Optimize getSystemService

--- app/src/main/java/org/hydev/wearsync/Extensions.kt
+++ app/src/main/java/org/hydev/wearsync/Extensions.kt
@@ -5,14 +5,15 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import com.google.android.material.snackbar.Snackbar
 
 fun View.snack(msg: String) = Snackbar.make(this, msg, Snackbar.LENGTH_LONG)
     .setAction("Action", null).show()
 
-fun Context.blueMan() = getSystemService(AppCompatActivity.BLUETOOTH_SERVICE) as BluetoothManager
+inline fun <reified T> Context.getSysServ() = getSystemService(T::class.java) as T
+fun Context.blueMan() = getSysServ<BluetoothManager>()
+
 val Context.pref get() = getSharedPreferences("settings", Context.MODE_PRIVATE)
 var Context.chosenDevice: String?
     get() = pref.getString("mac", null)
