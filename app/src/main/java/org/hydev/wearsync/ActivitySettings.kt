d146b2f3be9060d6b60bedacbd5d2ca5e034023a
Azalea Gui <me@hydev.org>
2023-01-23 19:41:23 -0500

    [+] Settings template

---
+++ app/src/main/java/org/hydev/wearsync/ActivitySettings.kt
@@ -0,0 +1,31 @@
+package org.hydev.wearsync
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class ActivitySettings : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        if (savedInstanceState == null)
+        {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat()
+    {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?)
+        {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
