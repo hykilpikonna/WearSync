5c8e526930c6e4e587ebdef2031bfaeec1761a91
Azalea Gui <me@hydev.org>
2023-01-23 22:17:18 -0500

    [O] Make measurement for battery

--- app/src/main/java/org/hydev/wearsync/bles/decoders/BatteryDecoder.kt
+++ app/src/main/java/org/hydev/wearsync/bles/decoders/BatteryDecoder.kt
@@ -1,15 +1,16 @@
 package org.hydev.wearsync.bles.decoders
 
+import com.influxdb.annotations.Measurement
 import com.welie.blessed.asUInt8
 import java.util.*
 
-class BatteryDecoder : IDecoder<UInt>
+@Measurement(name = "wear-battery")
+data class BatteryMeasurement(val percent: UInt)
+
+class BatteryDecoder : IDecoder<BatteryMeasurement>
 {
     override val sid = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
     override val cid = UUID.fromString("00002A19-0000-1000-8000-00805f9b34fb")
 
-    override fun decode(value: ByteArray): UInt
-    {
-        return value.asUInt8()!!
-    }
+    override fun decode(value: ByteArray) = BatteryMeasurement(value.asUInt8()!!)
 }
--- app/src/main/java/org/hydev/wearsync/bles/decoders/ModelDecoder.kt
+++ app/src/main/java/org/hydev/wearsync/bles/decoders/ModelDecoder.kt
@@ -8,8 +8,5 @@ class ModelDecoder : IDecoder<String>
     override val sid = UUID.fromString("0000180A-0000-1000-8000-00805f9b34fb")
     override val cid = UUID.fromString("00002A24-0000-1000-8000-00805f9b34fb")
 
-    override fun decode(value: ByteArray): String
-    {
-        return value.asString()
-    }
+    override fun decode(value: ByteArray) = value.asString()
 }
